6a6538d48760c573e889a747d363612a8997d04f
ARNAB9669 <arnabbiswas9669@gmail.com>
2025-11-11 10:57:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const card = document.getElementById("crd");
+    const flipBtn = document.getElementById("fp");
+
+    // Button flip
+    flipBtn.addEventListener("click", () => {
+        card.classList.toggle("is-flipped");
+    });
+
+    // Hover flip
+    card.addEventListener("mouseenter", () => {
+        card.classList.add("is-flipped");
+    });
+
+    card.addEventListener("mouseleave", () => {
+        card.classList.remove("is-flipped");
+
+    });
+
+    function show() {
+        document.querySelector(".card").classList.toggle("visible")
+        document.querySelector(".qr").classList.toggle("visible")
+    }
+
+    const QR = document.getElementById("code")
+    QR.addEventListener("click", ()=>{
+        show();
+    })
+
+
+
+    const redirect = document.getElementById("W")
+    redirect.addEventListener("click",()=>{
+        window.location.href = "https://codevelly.vercel.app/"
+    })
+})
+
